6f0515409830298a075774ca45f7237c04a75d51
Daniela Parra <dyparra47@gmail.com>
2019-02-25 18:27:40 -0500

    Add node class

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,3 +1,9 @@
+class Node:
+  def __init__(self,value=None):
+    self.value = value
+    self.left_child = None
+    self.right_child = None
+
 class BinarySearchTree:
   def __init__(self, value):
     self.value = value
