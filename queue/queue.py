11b7206f01b48f11a1182178e331698341ea5c5f
Daniela Parra <dyparra47@gmail.com>
2019-02-25 16:01:58 -0500

    Add linked list and node classes and implement queue class with linked list

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,23 @@
+
+from linked_list_py import LinkedList
+
 class Queue:
   def __init__(self):
     self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    self.storage = LinkedList()
 
   def enqueue(self, item):
-    pass
+    self.storage.add_to_tail(item)
   
   def dequeue(self):
-    pass
+    return self.storage.remove_head()
 
   def len(self):
-    pass
+    currNode = self.storage.head
+    count = 0
+    while currNode is not None:
+      nextNode = currNode.get_next()
+      count += 1
+      currNode = nextNode
+    
+    return count
