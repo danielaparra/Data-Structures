e255241c1a1fea6270ce67127423ef7951f1c3f6
Daniela Parra <dyparra47@gmail.com>
2019-02-26 16:38:58 -0500

    Update initializer and add insert, get max and get size methods

--- heap/heap.py
+++ heap/heap.py
@@ -1,18 +1,26 @@
 class Heap:
   def __init__(self):
-    self.storage = []
+    # Initialize storage array with None to start heap values at index 1.
+    self.storage = [None]
 
   def insert(self, value):
-    pass
+    # Add new value to end of array.
+    self.storage.append(value)
+    # Get value's current index.
+    curr_index = self.get_size() - 1
+    # Bubble up until it gets to the right position.
+    self._bubble_up(curr_index)
 
   def delete(self):
     pass
 
   def get_max(self):
-    pass
+    # Return first item that should be index 1.
+    return self.storage[1]
 
   def get_size(self):
-    pass
+    # Return length of array minus the item in index 0.
+    return len(self.storage) - 1
 
   def _bubble_up(self, index):
     pass
