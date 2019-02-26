7adbdfdd788148faf264576194e0b00b164ac18e
Daniela Parra <dyparra47@gmail.com>
2019-02-25 23:55:49 -0500

    Update contains and insert methods

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -11,15 +11,15 @@ class BinarySearchTree:
     self.right = None
 
   def _insertRecursive(self, curr_node, value):
-     # If value added is greater than root value, add to the right.
-    if curr_node.value > value:
+     # If value added is greater than current node value, add to the right.
+    if value > curr_node.value :
       # If current node's right child is None, set right child as new node with value.
       if not curr_node.right:
         curr_node.right = Node(value)
       # Otherwise call insert method again on current node's right child.
       else:
         self._insertRecursive(curr_node.right, value)
-    # If value is less than or equal to, add to the left.
+    # If value is less than or equal to current node value, add to the left.
     else:
       # If current node's left child is None, set left child as new node with value.
       if not curr_node.left:
@@ -29,20 +29,23 @@ class BinarySearchTree:
         self._insertRecursive(curr_node.left, value)
 
   def insert(self, value):
+    # If root value is None, set root value to given value.
+    if not self.value:
+      self.value = value
     # If value added is greater than root value, add to the right.
-    if value > self.value:
+    elif value > self.value:
       self._insertRecursive(self, value)
     # If value is less than or equal to, add to the left.
     else:
-      self._insertRecursive(self.left, value)
+      self._insertRecursive(self, value)
 
   def _containsRecursive(self, target, curr_node):
-    # If target equals current node value, return true.
-    if target == curr_node.value:
-      return True
     # If search reaches a current node to None, it could not find target and return False.
-    elif curr_node is None:
+    if curr_node is None:
       return False
+    # If target equals current node value, return true.
+    elif target == curr_node.value:
+      return True
     # If target is greater than current node value, recurse through right child.
     elif target > curr_node.value:
       return self._containsRecursive(target, curr_node.right)
@@ -56,10 +59,10 @@ class BinarySearchTree:
       return True
     # If target is greater than binary search tree root value, recurse through root's right child.
     elif target > self.value:
-      self._containsRecursive(target, self.right)
+      return self._containsRecursive(target, self.right)
     # If target is less than binary search tree root value, recurse through root's left child.
     else:
-      self._containsRecursive(target, self.left)
+      return self._containsRecursive(target, self.left)
 
   def _get_max_recursively(self, curr_node):
     if not curr_node.right:
