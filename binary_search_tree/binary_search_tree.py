014f8831f9d513008ee8f998795f3394fe603be2
Daniela Parra <dyparra47@gmail.com>
2019-02-25 22:52:04 -0500

    Add insert value method

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -10,11 +10,31 @@ class BinarySearchTree:
     self.left = None
     self.right = None
 
+  def _insertRecursive(self, curr_node, value):
+     # If value added is greater than root value, add to the right.
+    if curr_node.value > value:
+      # If current node's right child is None, set right child as new node with value.
+      if not curr_node.right:
+        curr_node.right = Node(value)
+      # Otherwise call insert method again on current node's right child.
+      else:
+        self._insertRecursive(curr_node.right, value)
+    # If value is less than or equal to, add to the left.
+    else:
+      # If current node's left child is None, set left child as new node with value.
+      if not curr_node.left:
+        curr_node.left = Node(value)
+      # Otherwise call insert method again on current node's left child.
+      else:
+        self._insertRecursive(curr_node.left, value)
+
   def insert(self, value):
     # If value added is greater than root value, add to the right.
     if value > self.value:
-      new_node = Node(value)
-      
+      self._insertRecursive(self, value)
+    # If value is less than or equal to, add to the left.
+    else:
+      self._insertRecursive(self.left, value)
 
   def contains(self, target):
     pass
